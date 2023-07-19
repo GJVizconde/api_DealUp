e3c70f1318e6d2dc7fd3790c4c503aff85efc17e
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2023-07-19 00:53:27 -0500

    postman & getAllUsers & getUserByName

--- src/controllers/Users/getAllUsers.js
+++ src/controllers/Users/getAllUsers.js
@@ -1,17 +1,9 @@
 const { User } = require('../../db');
-const axios = require('axios');
-const { cleanArray } = require('../../helpers/index');
 
 const getAllUsers = async () => {
   const dataBaseUsers = await User.findAll();
 
-  const apiUsersRaw = (
-    await axios.get(`https://jsonplaceholder.typicode.com/users`)
-  ).data;
-
-  const apiUsers = cleanArray(apiUsersRaw);
-
-  return [...dataBaseUsers, ...apiUsers];
+  return [...dataBaseUsers];
 };
 
 module.exports = getAllUsers;
