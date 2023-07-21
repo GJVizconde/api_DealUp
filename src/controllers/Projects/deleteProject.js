c6ef7abf1027ee8ba9b0eb2a36f7f7d18f80889a
Esteban900 <estebanandrusyszyn90@gmail.com>
2023-07-21 12:13:50 -0300

    delete project and finish crud

---
+++ src/controllers/Projects/deleteProject.js
@@ -0,0 +1,23 @@
+const { Project } = require('../../db');
+
+const deleteProject = async (id) => {
+  try {
+    projectFound = await Project.findByPk(id);
+
+    if (!projectFound) {
+      throw new Error('Project not found');
+    }
+
+    const deletedProject = await Project.destroy({
+      where: {
+        id,
+      },
+    });
+
+    return deletedProject;
+  } catch (error) {
+    throw new Error('Failed to delete project: ' + error.message);
+  }
+};
+
+module.exports = {deleteProject}
--- src/routes/projectRouter.js
+++ src/routes/projectRouter.js
@@ -3,11 +3,15 @@ const { Router } = require ( "express" );
 const { getProjectsHandler, getIdProjectsHandler }= require('../handlers/Projects/getProjectsHandler');
 const { createProjectHandler } = require('../handlers/Projects/postProjectsHandler');
 const { updateProjectHandler } = require('../handlers/Projects/putProjectHandler');
+const {deleteProjectHandler} = require('../handlers/Projects/deleteProjectHandler');
+
 const projectRouter = Router();
 
 projectRouter.get('/', getProjectsHandler);
 projectRouter.get('/:id', getIdProjectsHandler);
 projectRouter.post('/', createProjectHandler);
 projectRouter.put('/:id', updateProjectHandler);
+projectRouter.delete('/:id', deleteProjectHandler);
+
 
 module.exports = projectRouter;
