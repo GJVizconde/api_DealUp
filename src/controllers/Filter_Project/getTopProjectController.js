b322a15a35d4f095a85f749d663d43f776f61a8e
Esteban900 <estebanandrusyszyn90@gmail.com>
2023-08-04 02:34:34 -0300

    Finish filter ten project

--- src/controllers/Filter_Project/getTopProjectController.js
+++ src/controllers/Filter_Project/getTopProjectController.js
@@ -6,9 +6,16 @@ const topProjectController = async () => {
         
         const allProject = await getAllProjects();
 
-        const topRatedProjects = allProject.slice().sort( (a,b) => b.average_rating - a.average_rating ).slice(0, 3);
+        const topRatedProjects = allProject.slice().sort( (a,b) => b.average_rating - a.average_rating ).slice(0, 10);
 
-       // const latestProjects = allProject.slice().sort( (a,b) => )
+        const latestProjects = allProject.slice().sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate)).slice(0, 10);
+
+        const result = {
+            topRated: topRatedProjects,
+            last_created: latestProjects
+        };
+
+        return result;
 
     } catch (error) {
         throw error;
