7f31a8c69965049c9d108c20b79c337b69579782
Adiel <wereber.dragons14@live.com.mx>
2023-08-07 22:32:54 -0600

    mercado pago adiel

--- src/controllers/PaymentController/paymentController.js
+++ src/controllers/PaymentController/paymentController.js
@@ -1,42 +1,50 @@
-const mercadopago = require('mercadopago');
-const { Investment } = require('../../db');
-const createrOrder = async (req,res) => {
-const HOST ="http://localhost:3001";
-    const { contribution, ProjectId, UserId } = req.body;
+const mercadopago = require("mercadopago");
+const { Investment } = require("../../db");
+const createrOrder = async (req, res) => {
+  const HOST = "http://localhost:3001";
+  const { contribution, ProjectId, UserId, comment } = req.body;
 
-    const newInvestment = await createInvestment(contribution, comment, UserId, ProjectId);
-    console.log("new investment pending",newInvestment);
-    try {
-        mercadopago.configure({
-            access_token:"",
-        });
+  console.log({ contribution, ProjectId, UserId });
+  const newInvestment = await Investment.create({
+    contribution,
+    comment,
+    ProjectId,
+    UserId,
+  });
 
-        const result = await mercadopago.preferences.create({
-            items:[
-               {
-                 title: "",
-                unit_price: contribution,
-                currency_id:'USD',
-                quantity: 1,
-            }
-            ],
-            back_urls: {
-                success: `${HOST}/success?ProjectId=${newInvestment.id}`,
-                failure: `${HOST}/failure`,
-                pending: `${HOST}/pending`,
-            },
-            auto_return: 'approved',
-            payment_methods: {
-                installments: 1,
-            },
-            notification_url: "https://2349-201-252-167-59.ngrok.io/webhook",
-        });
-        console.log(result);
-        res.send(result.body);
-    } catch (error) {
-        res.status(500).json({ error: error.message})
-    }
+  console.log("new investment pending", newInvestment);
+  try {
+    mercadopago.configure({
+      access_token:
+        "TEST-3380620890938082-080521-71b737ea9b6f8b8db1ea2949c6d6e778-1443137702",
+    });
 
+    const result = await mercadopago.preferences.create({
+      items: [
+        {
+          title: "",
+          unit_price: contribution,
+          currency_id: "USD",
+          quantity: 1,
+        },
+      ],
+      back_urls: {
+        success: `${HOST}/success?ProjectId=${newInvestment.id}`,
+        failure: `${HOST}/failure`,
+        pending: `${HOST}/pending`,
+      },
+      auto_return: "approved",
+      payment_methods: {
+        installments: 1,
+      },
+      notification_url:
+        "https://797e-2806-10a6-20-577e-552c-36fc-7e69-3d5b.ngrok.io/webhook",
+    });
+    console.log(result);
+    res.send(result.body);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
-module.exports = { createrOrder }
+module.exports = { createrOrder };
--- src/db.js
+++ src/db.js
@@ -1,25 +1,25 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const UserModel = require('./models/User');
-const ProjectModel = require('./models/Project');
-const RatingModel = require('./models/Rating');
-const GalleryModel = require('./models/Gallery');
-const PostModel = require('./models/Post');
-const CommentModel = require('./models/Comment');
-const InvestmentModel = require('./models/Investment');
-const ImageModel = require('./models/Image');
+const UserModel = require("./models/User");
+const ProjectModel = require("./models/Project");
+const RatingModel = require("./models/Rating");
+const GalleryModel = require("./models/Gallery");
+const PostModel = require("./models/Post");
+const CommentModel = require("./models/Comment");
+const InvestmentModel = require("./models/Investment");
+const ImageModel = require("./models/Image");
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY, NODE_ENV } = process.env;
 
 let sequelize;
-if (NODE_ENV === 'development') {
+if (NODE_ENV === "development") {
   sequelize = new Sequelize(
     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dealupdb`,
     {
       logging: false,
       native: false,
-      charset: 'utf8mb4',
+      charset: "utf8mb4",
       define: {
         validate: true,
       },
@@ -27,7 +27,7 @@ if (NODE_ENV === 'development') {
   );
 } else {
   sequelize = new Sequelize(DB_DEPLOY, {
-    dialect: 'postgres',
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
@@ -37,7 +37,7 @@ if (NODE_ENV === 'development') {
     },
     logging: false,
     native: false,
-    charset: 'utf8mb4',
+    charset: "utf8mb4",
     ssl: true,
   });
 }
@@ -51,52 +51,49 @@ GalleryModel(sequelize);
 InvestmentModel(sequelize);
 ImageModel(sequelize);
 
-const { User, Project, Rating, Gallery, Comment, Post, Image, Investment } = sequelize.models;
+const { User, Project, Rating, Gallery, Comment, Post, Image, Investment } =
+  sequelize.models;
 
 //relacion de tablas
 
 //USER-PROJECT
-User.belongsToMany(Project, { through: 'user_project' });
-Project.belongsToMany(User, { through: 'user_project' });
+User.belongsToMany(Project, { through: "user_project" });
+Project.belongsToMany(User, { through: "user_project" });
 
 //USER-RATING
 
-User.hasOne(Rating, { onUpdate: 'CASCADE'});
-Rating.belongsTo(User, {onDelete: 'CASCADE', onUpdate: 'CASCADE',});
+User.hasOne(Rating, { onUpdate: "CASCADE" });
+Rating.belongsTo(User, { onDelete: "CASCADE", onUpdate: "CASCADE" });
 
 // USER-COMMENT
-User.hasMany(Comment, { onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+User.hasMany(Comment, { onDelete: "CASCADE", onUpdate: "CASCADE" });
 Comment.belongsTo(User);
 
 //PROJECT-RATING
-Project.hasMany(Rating, { onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+Project.hasMany(Rating, { onDelete: "CASCADE", onUpdate: "CASCADE" });
 Rating.belongsTo(Project);
 
 //PROJECT-GALLERY
-Project.hasMany(Gallery, { onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+Project.hasMany(Gallery, { onDelete: "CASCADE", onUpdate: "CASCADE" });
 Gallery.belongsTo(Project);
 
 //PROJECT-INVESTMENT
 
-Project.hasMany(Investment, { onUpdate: 'CASCADE' });
+Project.hasMany(Investment, { onUpdate: "CASCADE" });
 Investment.belongsTo(Project);
 
-
 //INVESTMENT-USER
-Investment.belongsToMany(User, { through: 'investment_user' });
-User.belongsToMany(Investment, { through: 'investment_user' });
-
+User.hasMany(Investment, { onUpdate: "CASCADE" });
+Investment.belongsTo(User);
 
 //PROJECT-POST
-Project.hasMany(Post, { onDelete: 'CASCADE', onUpdate: 'CASCADE' }),
+Project.hasMany(Post, { onDelete: "CASCADE", onUpdate: "CASCADE" }),
   Post.belongsTo(Project);
 
 //POST-COMMENT
 
-Post.hasMany(Comment, { onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+Post.hasMany(Comment, { onDelete: "CASCADE", onUpdate: "CASCADE" });
 Comment.belongsTo(Post);
-
-
 
 module.exports = {
   ...sequelize.models,
